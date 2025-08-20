d92c5eaae74bcf84f19fb7b2d909ee51c848d787
apinedao <apinedao@homecenter.co>
2025-08-19 19:06:52 -0500

    Add Ip Server

--- src/app/services/ftp.service.ts
+++ src/app/services/ftp.service.ts
@@ -7,7 +7,7 @@ import * as CryptoJS from 'crypto-js';
 @Injectable({ providedIn: 'root' })
 export class FtpService {
   constructor(private http: HttpClient) {}
-  private apiUrl = 'http://localhost:7000'; // Cambia si usas otro puerto/backend
+  private apiUrl = 'http://179.50.21.74:7000'; // Cambia si usas otro puerto/backend
   private secretKey = '5ecf6fd301f27a5f91d2502d5342075aa4dfee0db9a3feabf24bcc485d710df3c8e7d301cee592cf59b1ba17b6a6ad96e7acfd284bf9bfcfff7f128e9fee04cc';
 
   login(user: { username: string; password: string }) {
